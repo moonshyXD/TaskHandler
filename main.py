ad542df719edabdd879fe2a8d7bf101bf77bb857
moonshy <vladbobkb10@gmail.com>
2026-03-03 19:53:32 +0300

    [feature] add logger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.adapters.cli import CLI
+from src.adapters.logger import logging
 from src.adapters.repository import TaskRepository
 from src.domain.errors import ContractViolationError, InputValidationError
 from src.usecases.import_tasks import ImportTasks
@@ -8,41 +9,55 @@ from src.usecases.task_random import TaskRandom
 
 
 def run() -> None:
-    """
-    Запуск интерактивного обработчика задач
-    """
+    """Запуск интерактивного обработчика задач"""
+    logging.info("Начало работы программы")
     CLI.greet()
+
     repo = TaskRepository()
     importer = ImportTasks(repository=repo)
 
     while (action := CLI.action_select()) != CLI.EXIT:
         try:
+            logging.info(f"Пользователь выбрал событие: {action}")
+            logging.info(f"Начало выполнения события: {action}")
+
             match action:
                 case CLI.TASKS_FROM_FILE:
                     file_path = CLI.get_file_path()
-                    tasks_file_obj = TaskFile(file_path=file_path)
-                    importer.execute(tasks_file_obj)
+                    logging.info(
+                        f"Пользователь ввел путь к файлу: {file_path}"
+                    )
+                    importer.execute(TaskFile(file_path=file_path))
 
                 case CLI.TASKS_RANDOM:
-                    tasks_count = CLI.get_tasks_count()
-                    tasks_random_obj = TaskRandom(tasks_count=tasks_count)
-                    importer.execute(tasks_random_obj)
+                    count = CLI.get_tasks_count()
+                    logging.info(f"Пользователь ввел кол-во заданий: {count}")
+                    importer.execute(TaskRandom(tasks_count=count))
 
                 case CLI.TASKS_API:
-                    tasks_api_obj = TaskAPI()
-                    importer.execute(tasks_api_obj)
+                    importer.execute(TaskAPI())
 
                 case CLI.CHECK_TASKS:
                     saved_tasks = repo.get_all()
                     CLI.print_tasks(tasks=saved_tasks)
 
+            logging.info(f"Успешно выполнено событие: {action}")
+
         except InputValidationError as e:
+            logging.error(f"Ошибка ввода: {e}")
             print(f"Ошибка ввода: \n{e}")
+
         except ContractViolationError as e:
-            print(f"Ошибка системы \n{e}")
+            logging.error(f"Ошибка системы: {e}")
+            print(f"Ошибка системы: \n{e}")
+
         except Exception as e:
+            logging.error(
+                f"Произошла непредвиденная ошибка: {e}", exc_info=True
+            )
             print(f"Произошла непредвиденная ошибка: \n{e}")
 
+    logging.info("Окончание работы программы")
     CLI.goodbye()
 
 
